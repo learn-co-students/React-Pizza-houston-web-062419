977877977614c840d17913deb21a5a80521eb62b
Varvara Babkova <varvara.munday@gmail.com>
2019-09-04 08:39:43 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,57 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			pizzas:[],
+			edit_pizza:{"id": 0,
+					    "topping": "",
+					    "size": "",
+					    "vegetarian": true
+					}
+		}
+	}
+
+	componentDidMount(){
+		fetch("http://localhost:3000/pizzas")
+		.then(res => res.json())
+		.then(res => this.setState({pizzas: res}))
+	}
+
+	handleEdit = (pizza) =>{
+		this.setState({edit_pizza: pizza}, ()=>console.log(this.state.edit_pizza))
+	} 
+
+	handleSubmit = (e, t)=>{
+		e.preventDefault()
+		console.log(e.target.children)
+		console.log(t)
+
+		let configObj = {
+                method: "PATCH",
+                headers: {
+                 "Content-Type": "application/json",
+                  "Accept": "application/json"
+                  },
+                 body: JSON.stringify(t)
+        };
+
+		fetch("http://localhost:3000/pizzas/"+t.id, configObj)
+
+		let empty_pizza = {"id": 0,
+					    "topping": "",
+					    "size": "",
+					    "vegetarian": true}
+		this.setState({edit_pizza:empty_pizza})
+	}
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm pizza={this.state.edit_pizza} handleSubmit = {this.handleSubmit}/>
+        <PizzaList pizzas = {this.state.pizzas} handleEdit = {this.handleEdit}/>
       </Fragment>
     );
   }
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -15,7 +15,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.props.pizzas.map(p => <Pizza pizza={p} handleEdit = {this.props.handleEdit}/>)
           }
         </tbody>
       </table>
